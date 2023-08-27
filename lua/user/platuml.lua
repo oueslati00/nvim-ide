8d35d6c4f121dc32ec73966fe4b17cc6792c82d1
brahim Oueslati <boueslati76@gmail.com>
2023-08-27 03:00:29 +0100

    add plantuml plugins

---
+++ lua/user/platuml.lua
@@ -0,0 +1,13 @@
+return  {
+  {  "aklt/plantuml-syntax", 
+    ft = "plantuml", 
+    keys = {
+        { "<leader>po", ":PlantumlStart<cr>"},
+        {"<leader>ps",":PlantumlSave<cr>"},
+    }
+  },
+  {"weirongxu/plantuml-previewer.vim" ,  ft ="plantuml"
+},
+    {"tyru/open-browser.vim",  ft ="plantuml"
+},
+  }
