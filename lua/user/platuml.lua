32d2855932f46ba4117b400b8d4146a7e9bfb6c4
brahim Oueslati <boueslati76@gmail.com>
2023-09-13 10:49:37 +0100

    formating code

--- lua/user/platuml.lua
+++ lua/user/platuml.lua
@@ -1,13 +1,12 @@
-return  {
-  {  "aklt/plantuml-syntax", 
-    ft = "plantuml", 
+return {
+  {
+    "aklt/plantuml-syntax",
+    ft = "plantuml",
     keys = {
-        { "<leader>po", ":PlantumlStart<cr>"},
-        {"<leader>ps",":PlantumlSave<cr>"},
-    }
+      { "<leader>po", ":PlantumlStart<cr>" },
+      { "<leader>ps", ":PlantumlSave<cr>" },
+    },
   },
-  {"weirongxu/plantuml-previewer.vim" ,  ft ="plantuml"
-},
-    {"tyru/open-browser.vim",  ft ="plantuml"
-},
-  }
+  { "weirongxu/plantuml-previewer.vim", ft = "plantuml" },
+  { "tyru/open-browser.vim", ft = "plantuml" },
+}
